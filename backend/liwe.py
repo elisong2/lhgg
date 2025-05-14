befa1e01f14bc7ebf296e9cb293230c82307eb2c
Eli Song <eli.ehnj2@gmail.com>
2025-05-13 20:19:39 -0400

    first commit

---
+++ backend/liwe.py
@@ -0,0 +1,14 @@
+from user import user
+
+def main():
+    eli = user("OPMAGEMASTER", "#NA1")
+    # ethan = user("mingoose9", "#NA1")
+    # seb = user('정성화', '#5127')
+    
+    eli.update()
+
+    return 0
+    
+
+if __name__ == "__main__":
+    main()
